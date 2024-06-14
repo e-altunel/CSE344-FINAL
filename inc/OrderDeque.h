59b9b9f4b0ed2dc88485683edcfe846321cf9033
e-altunel <demirhan.altunel+github@gmail.com>
2024-06-14 14:44:44 +0300

    refactor: Modify t_OrderDeque_enqueue and t_OrderDeque_dequeue to support non-blocking mode
    The functions t_OrderDeque_enqueue and t_OrderDeque_dequeue have been modified to support a new non-blocking mode. The t_OrderRequestMode parameter has been added to both functions to specify the mode of operation. In the enqueue function, if the mode is set to non-blocking and the deque is full, the function will return immediately without waiting. In the dequeue function, if the mode is set to non-blocking and the deque is empty, the function will return immediately without waiting. These changes provide more flexibility in how orders are enqueued and dequeued from the deque.

--- inc/OrderDeque.h
+++ inc/OrderDeque.h
@@ -15,9 +15,20 @@ typedef struct s_OrderDeque {
   sem_t           full_slots;
 } t_OrderDeque;
 
+typedef enum e_OrderDequeStatus {
+  ORDER_DEQUE_OK,
+  ORDER_DEQUE_ERROR
+} t_OrderDequeStatus;
+
+typedef enum e_OrderRequestMode {
+  ORDER_REQUEST_MODE_BLOCKING,
+  ORDER_REQUEST_MODE_NON_BLOCKING
+} t_OrderRequestMode;
+
 int  t_OrderDeque_init(t_OrderDeque *deque, unsigned int size);
 void t_OrderDeque_destroy(t_OrderDeque *deque);
-int  t_OrderDeque_enqueue(t_OrderDeque *deque, t_Order *order);
-int  t_OrderDeque_dequeue(t_OrderDeque *deque, t_Order *order);
+int  t_OrderDeque_enqueue(t_OrderDeque *deque, t_Order *order, t_OrderRequestMode mode);
+int  t_OrderDeque_dequeue(t_OrderDeque *deque, t_Order **order, t_OrderRequestMode mode);
+int  t_OrderDeque_size(t_OrderDeque *deque);
 
 #endif /* INC_ORDERDEQUE */
--- src/OrderDeque.c
+++ src/OrderDeque.c
@@ -1,4 +1,5 @@
 #include <OrderDeque.h>
+#include <stdlib.h>
 
 int t_OrderDeque_init(t_OrderDeque *deque, unsigned int size) {
   if (deque == 0)
@@ -44,12 +45,22 @@ void t_OrderDeque_destroy(t_OrderDeque *deque) {
     free(deque->orders);
 }
 
-int t_OrderDeque_enqueue(t_OrderDeque *deque, t_Order *order) {
+int t_OrderDeque_enqueue(t_OrderDeque *deque, t_Order *order, t_OrderRequestMode mode) {
   if (deque == 0 || order == 0)
     return -1;
 
+  if (mode == ORDER_REQUEST_MODE_NON_BLOCKING)
+    if (sem_trywait(&deque->empty_slots) != 0)
+      return -1;
+
   sem_wait(&deque->empty_slots);
   pthread_mutex_lock(&deque->mutex);
+
+  if ((deque->tail + 1) % deque->size == deque->head) {
+    pthread_mutex_unlock(&deque->mutex);
+    sem_post(&deque->empty_slots);
+    return -1;
+  }
 
   deque->orders[deque->tail] = *order;
   deque->tail                = (deque->tail + 1) % deque->size;
@@ -60,18 +71,46 @@ int t_OrderDeque_enqueue(t_OrderDeque *deque, t_Order *order) {
   return 0;
 }
 
-int t_OrderDeque_dequeue(t_OrderDeque *deque, t_Order *order) {
-  if (deque == 0 || order == 0)
+int t_OrderDeque_dequeue(t_OrderDeque *deque, t_Order **order, t_OrderRequestMode mode) {
+  if (deque == 0 || order == 0 || *order != 0)
+    return -1;
+
+  int temp_sem_value = 0;
+
+  sem_getvalue(&deque->full_slots, &temp_sem_value);
+  if (mode == ORDER_REQUEST_MODE_NON_BLOCKING && temp_sem_value == 0)
     return -1;
 
   sem_wait(&deque->full_slots);
   pthread_mutex_lock(&deque->mutex);
 
-  *order      = deque->orders[deque->head];
+  if (deque->head == deque->tail) {
+    pthread_mutex_unlock(&deque->mutex);
+    return -1;
+  }
+
+  *order = (t_Order *)malloc(sizeof(t_Order));
+  if (*order == 0) {
+    pthread_mutex_unlock(&deque->mutex);
+    sem_post(&deque->full_slots);
+    return -1;
+  }
+  **order     = deque->orders[deque->head];
   deque->head = (deque->head + 1) % deque->size;
 
   pthread_mutex_unlock(&deque->mutex);
   sem_post(&deque->empty_slots);
 
   return 0;
+}
+
+int t_OrderDeque_size(t_OrderDeque *deque) {
+  if (deque == 0)
+    return -1;
+
+  pthread_mutex_lock(&deque->mutex);
+  int size = (deque->tail - deque->head + deque->size) % deque->size;
+  pthread_mutex_unlock(&deque->mutex);
+
+  return size;
 }
